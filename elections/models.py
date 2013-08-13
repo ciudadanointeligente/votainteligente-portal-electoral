77fdaa875d892c6bcce13223c8783b958638c6b7
Felipe Álvarez <luisfelipealvarez@gmail.com>
2013-08-13 12:08:05 -0400

    Adding basic election model

--- elections/models.py
+++ elections/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Election(models.Model):
+	name = models.CharField(max_length=255)
+	slug = models.CharField(max_length=255, unique=True)
+	description = models.CharField(max_length=255)
---
+++ elections/tests/election_tests.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+from elections.models import Election
+from django.db import IntegrityError
+
+
+class ElectionTestCase(TestCase):
+	def setUp(self):
+		super(ElectionTestCase, self).setUp()
+
+	def test_election_create(self):
+		election = Election.objects.create(
+			name='the name',
+			slug='the-slug',
+			description='this is a description'
+			)
+		self.assertEquals(election.name, 'the name')
+		self.assertEquals(election.slug, 'the-slug')
+		self.assertEquals(election.description, 'this is a description')
+
+	def test_there_are_no_two_elections_with_the_same_slug(self):
+		Election.objects.create(
+			slug='the-slug',
+			)
+		with self.assertRaises(IntegrityError):
+			Election.objects.create(
+				slug='the-slug',
+				)
+
+

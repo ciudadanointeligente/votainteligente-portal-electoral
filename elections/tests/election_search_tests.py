693f75d1621608d4ef2d98fd72165b37b25a356b
Felipe Álvarez <luisfelipealvarez@gmail.com>
2013-08-14 18:51:21 -0400

    Fixes example data and makesit completely searchable

--- elections/tests/election_search_tests.py
+++ elections/tests/election_search_tests.py
@@ -26,9 +26,9 @@ class ElectionIndexTestCase(TestCase):
 		indexed_election = index.text.prepare_template(election)
 
 		self.assertIn(election.name, indexed_election)
-		self.assertIn(u'Nueva Imperial', indexed_election)
-		self.assertIn(u'Pitrufquén', indexed_election)
-		self.assertIn(u'Saavedra', indexed_election)
+		self.assertIn(u'\nNueva Imperial\n', indexed_election)
+		self.assertIn(u'\nPitrufquén\n', indexed_election)
+		self.assertIn(u'\nSaavedra\n', indexed_election)
 
 
 class ElectionSearchFormTestCase(TestCase):

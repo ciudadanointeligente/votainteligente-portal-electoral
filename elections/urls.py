009a2cfbe759c9d7420b0ee5dea86a4e458dbbc1
Felipe Álvarez <luisfelipealvarez@gmail.com>
2013-08-19 18:03:34 -0400

    Adding the form to the home view

--- elections/urls.py
+++ elections/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
 from haystack.views import SearchView
 from elections.forms import ElectionForm
-from elections.views import ElectionsSearchByTagView
+from elections.views import ElectionsSearchByTagView, HomeView
 
 urlpatterns = patterns('',
-	url(r'^/?$', TemplateView.as_view(template_name='elections/home.html'), name='home' ),
+	url(r'^/?$', HomeView.as_view(template_name='elections/home.html'), name='home' ),
 	url(r'^buscar/?$', SearchView(
 	        template='search.html',
 	        form_class=ElectionForm

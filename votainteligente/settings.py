90941c51645f9f224b4031450102f9bdc6898e0a
Felipe Álvarez <luisfelipealvarez@gmail.com>
2013-09-06 13:02:25 -0400

    Adding popit persons

--- votainteligente/settings.py
+++ votainteligente/settings.py
@@ -128,6 +128,7 @@ INSTALLED_APPS = (
     'haystack',
     'elections',
     'candideitorg',
+    'popit',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:

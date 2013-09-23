a9142982eb87034b6f54d7898ea25509dcc9deda
José Damián Garrido Muñoz <meine@piuke>
2013-09-23 12:49:29 -0300

    implementation of thumbnails correction of photo candidate example data add url for thumbnails update of gitignore

--- votainteligente/settings.py
+++ votainteligente/settings.py
@@ -1,5 +1,6 @@
 import sys
 # Django settings for votainteligente project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -7,6 +8,8 @@ TEMPLATE_DEBUG = DEBUG
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 MANAGERS = ADMINS
 
@@ -51,12 +54,12 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, '..' ,'cache')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+MEDIA_URL = '/cache/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -130,6 +133,7 @@ INSTALLED_APPS = (
     'candideitorg',
     'markdown_deux',
     'django_extensions',
+    'sorl.thumbnail',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:

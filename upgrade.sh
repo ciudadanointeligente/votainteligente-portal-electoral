8116bb62eda407303673dc889b7900416ea1b83c
Felipe Álvarez <luisfelipealvarez@gmail.com>
2013-09-25 15:30:03 -0300

    Adds upgrade script for ubuntu

--- upgrade.sh
+++ upgrade.sh
@@ -1,4 +1,13 @@
-mv votainteligente.db vi_backup.db
+#!/bin/bash
+set -e
+export DB_NAME="votainteligente.db"
+
+if [ -e $DB_NAME ]; then mv $DB_NAME vi_backup.db; fi
+pip install -r requirements.txt
 python manage.py syncdb --noinput
 python manage.py migrate
 python manage.py loaddata example_data.yaml
+
+
+./start_local_popit_api.bash
+./start_local_writeit.bash

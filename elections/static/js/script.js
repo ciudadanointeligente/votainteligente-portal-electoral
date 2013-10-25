40c4c1f32533bb3711c0ccf3e71c6f70cbc6f705
jdgarrido <damian.garrido.munoz@gmail.com>
2013-10-25 10:48:01 -0300

    fix counter

--- elections/static/js/script.js
+++ elections/static/js/script.js
@@ -7,7 +7,7 @@ $(function(){
 	if((new Date()) > ts){
 		// The new year is here! Count towards something else.
 		// Notice the *1000 at the end - time must be in milliseconds
-		ts = (new Date()).getTime() + 2*21*60*60*1000;
+		ts = new Date(2013, 09, 25);
 		newYear = false;
 	}
 		

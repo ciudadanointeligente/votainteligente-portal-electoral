d0a747f93f4c48ce4586cc3c45ffa3fe5786ddc3
Marcel Augsburger <maugsburger@gmail.com>
2013-11-14 18:11:11 -0300

    Added prepending url option for photo_loader

--- elections/management/commands/photo_loader.py
+++ elections/management/commands/photo_loader.py
@@ -4,17 +4,20 @@ from candideitorg.models import Candidate
 import csv
 
 class Command(BaseCommand):
-    args = '<candidate photos csv file>'
+    args = '<candidate photos csv file [,prepended url]>'
 
     def handle(self, *args, **options):
         file_path = args[0]
+        prepended_url = ''
+        if len(args) > 1:
+            prepended_url += args[1]            
         tags_lines = csv.reader(open(file_path, 'rb'), delimiter=',')
         tags_lines.next()
         for line in tags_lines:
             candidate_name = line[0].decode('utf-8').strip()
             try:
                 candidate = Candidate.objects.get(name=candidate_name)
-                photo_url = line[1].decode('utf-8').strip()
+                photo_url = prepended_url + line[1].decode('utf-8').strip()
                 candidate.photo = photo_url
                 candidate.save()
             except Candidate.DoesNotExist:

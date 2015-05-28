2ee461a3a1e0a57285742a5c252fd3521defe7f5
Felipe Álvarez <falvarez@votainteligente.cl>
2015-05-28 17:34:57 -0300

    Adding an admin for extrainformation in elections

--- elections/admin.py
+++ elections/admin.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from elections.models import Election, Candidate, PersonalData
 from popolo.models import Organization, Membership, ContactDetail, OtherName, Post, Area
 from django.contrib.contenttypes.admin import GenericTabularInline
+from django import forms
+from django.conf import settings
 # from django.contrib.flatpages.admin import FlatPageAdmin
 # from django.contrib.flatpages.models import FlatPage
 ## OOPS this is a custom widget that works for initializing
@@ -12,9 +14,40 @@ from django.contrib.contenttypes.admin import GenericTabularInline
 # from tinymce.widgets import TinyMCE
 
 
+class ElectionModelForm(forms.ModelForm):
+    model = Election
+
+    def __init__(self, *args, **kwargs):
+        super(ElectionModelForm, self).__init__(*args, **kwargs)
+        self.extra_info_fields = settings.DEFAULT_ELECTION_EXTRA_INFO.keys()
+        for key in self.extra_info_fields:
+            self.fields.update({key: forms.CharField(max_length=512,
+                                                     label=key,
+                                                     required=False,
+                                                     widget=forms.TextInput(),
+                                                     initial=self.instance.extra_info[key]
+                                                     )})
+
+    def save(self, commit=True, *args, **kwargs):
+        instance = super(ElectionModelForm, self).save(commit, *args, **kwargs)
+        for key in self.extra_info_fields:
+            instance.extra_info[key] = self.cleaned_data.get(key, None)
+        if commit:
+            instance.save()
+        return instance
+
+
 class ElectionAdmin(admin.ModelAdmin):
+    form = ElectionModelForm
     search_fields = ['name', 'tags']
 
+    def get_fieldsets(self, request, obj=None):
+        fieldsets = super(ElectionAdmin, self).get_fieldsets(request, obj)
+        if hasattr(request, "_gfs_marker"):
+            for key in settings.DEFAULT_ELECTION_EXTRA_INFO.keys():
+                fieldsets[0][1]['fields'] += (key,)
+        setattr(request, "_gfs_marker", 1)
+        return fieldsets
 
 admin.site.register(Election, ElectionAdmin)
 

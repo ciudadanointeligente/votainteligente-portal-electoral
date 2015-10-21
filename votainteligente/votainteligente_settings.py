c099bafe86fd14b1bca2a45bfa65ae5b4571c334
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2015-10-21 12:53:00 -0300

    If you use the following line DONT_USE_MIGRATIONS=1 ./manage.py test elections when running tests it will speed up tests

--- votainteligente/votainteligente_settings.py
+++ votainteligente/votainteligente_settings.py
@@ -221,6 +221,32 @@ NEW_ANSWER_ENDPOINT = 'NEW_ANSWER_ENDPOINT'
 
 THEME = None
 
+
+#Testing without migrations
+from django.conf import settings
+from django_nose import NoseTestSuiteRunner
+
+
+class DisableMigrations(object):
+    def __contains__(self, item):
+        return True
+
+    def __getitem__(self, item):
+        return "notmigrations"
+
+
+class Runner(NoseTestSuiteRunner):
+    def run_tests(self, test_labels, extra_tests=None):
+        settings.MIGRATION_MODULES = DisableMigrations()
+        super(Runner, self).run_tests(test_labels, extra_tests=extra_tests)
+
+
+DONT_USE_MIGRATIONS = 'DONT_USE_MIGRATIONS' in os.environ.keys() and os.environ['DONT_USE_MIGRATIONS'] == '1'
+
+if DONT_USE_MIGRATIONS:
+    TEST_RUNNER = 'votainteligente.votainteligente_settings.Runner'
+
+
 try:
     from local_settings import *
 except ImportError, e:

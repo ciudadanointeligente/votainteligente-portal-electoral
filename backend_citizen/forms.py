6a7f368af8e0312154cc0986b5fbe1ec41dbc971
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2016-04-26 13:38:25 -0300

    Showing organization images

--- backend_citizen/forms.py
+++ backend_citizen/forms.py
@@ -2,10 +2,12 @@ from django import forms
 from popular_proposal.models import Organization, Enrollment
 from votainteligente.facebook_page_getter import facebook_getter
 from images.models import Image
+from django.core.files.base import ContentFile
+import requests
 
 
 class OrganizationForm(forms.ModelForm):
-    facebook_page = forms.URLField()
+    facebook_page = forms.URLField(required=False)
 
     def __init__(self, *args, **kwargs):
         self.user = kwargs.pop('user')
@@ -14,8 +16,12 @@ class OrganizationForm(forms.ModelForm):
     def save(self, force_insert=False, force_update=False, commit=True):
         organization = super(OrganizationForm, self).save()
         if 'facebook_page' in self.cleaned_data and self.cleaned_data['facebook_page']:
+
             result = facebook_getter(self.cleaned_data['facebook_page'])
-            Image.objects.create(content_object=organization, source=result['picture_url'])
+            image_content = ContentFile(requests.get(result['picture_url']).content)
+
+            image = Image.objects.create(content_object=organization, source=result['picture_url'])
+            image.image.save(organization.id, image_content)
             organization.description = result['about']
             organization.save()
         Enrollment.objects.create(organization=organization,

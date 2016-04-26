de19fb5cd7f8b28795ca4a0aa4af2e6fa841b0be
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2016-04-26 18:30:34 -0300

    Updating facebook page getter and showing information abotu the organization

--- backend_citizen/forms.py
+++ backend_citizen/forms.py
@@ -1,13 +1,17 @@
+# coding=utf-8
 from django import forms
 from popular_proposal.models import Organization, Enrollment
 from votainteligente.facebook_page_getter import facebook_getter
 from images.models import Image
 from django.core.files.base import ContentFile
 import requests
+from django.utils.translation import ugettext as _
+from popolo.models import ContactDetail
 
 
 class OrganizationForm(forms.ModelForm):
-    facebook_page = forms.URLField(required=False)
+    facebook_page = forms.URLField(required=False,
+                                   label=_(u'¿Tienes una página en facebook?'))
 
     def __init__(self, *args, **kwargs):
         self.user = kwargs.pop('user')
@@ -16,7 +20,6 @@ class OrganizationForm(forms.ModelForm):
     def save(self, force_insert=False, force_update=False, commit=True):
         organization = super(OrganizationForm, self).save()
         if 'facebook_page' in self.cleaned_data and self.cleaned_data['facebook_page']:
-
             result = facebook_getter(self.cleaned_data['facebook_page'])
             image_content = ContentFile(requests.get(result['picture_url']).content)
 
@@ -24,6 +27,12 @@ class OrganizationForm(forms.ModelForm):
             image.image.save(organization.id, image_content)
             organization.description = result['about']
             organization.save()
+            c = ContactDetail(content_object=organization,
+                              contact_type=ContactDetail.CONTACT_TYPES.facebook,
+                              value=self.cleaned_data['facebook_page'],
+                              label=result['name'])
+            c.save()
+
         Enrollment.objects.create(organization=organization,
                                   user=self.user)
         return organization
--- votainteligente/facebook_page_getter.py
+++ votainteligente/facebook_page_getter.py
@@ -13,7 +13,7 @@ def facebook_getter(url):
 
     graph = facebook.GraphAPI(access_token=settings.FACEBOOK_ACCESS_TOKEN, version='2.5')
     params = {
-        'fields': 'about,picture,events'
+        'fields': 'about,picture,events,name'
     }
     url_parts = list(urlparse.urlparse(url))
     query = dict()
@@ -22,10 +22,15 @@ def facebook_getter(url):
     url = urlparse.urlunparse(url_parts)
     _object = graph.get_object(url)
     _id = _object.get('id')
+    _about = unicode(_object.get('about', ''))
+    _events = _object.get('events', [])
+    _picture_url = _object.get('picture', []).get('data', []).get('url','' )
+    _name = _object.get('name', 'Facebook Page')
     return {
-        'about': unicode(_object['about']),
+        'about': _about,
         'id': _id,
         'picture_url': _object['picture']['data']['url'],
-        'events': _object['events']
+        'events': _events,
+        'name': _name,
     }
 

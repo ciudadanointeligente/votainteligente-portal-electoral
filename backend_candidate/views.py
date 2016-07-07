70384637322b01c5c4390cdbed52ce4dbf0b6afb
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2016-07-07 13:56:50 -0400

    Using base class, separating responsabilities

--- backend_candidate/views.py
+++ backend_candidate/views.py
@@ -2,19 +2,22 @@ from django.shortcuts import render
 from backend_candidate.models import is_candidate
 from django.http import Http404
 from django.views.generic.base import TemplateView
+from django.views.generic import View
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 
 
-class HomeView(TemplateView):
-    template_name = "backend_candidate/home.html"
-
+class BackendCandidateBase(View):
     @method_decorator(login_required)
     def dispatch(self, request, *args, **kwargs):
         if not is_candidate(request.user):
             raise Http404
         self.user = request.user
-        return super(HomeView, self).dispatch(request, *args, **kwargs)
+        return super(BackendCandidateBase, self).dispatch(request, *args, **kwargs)
+
+
+class HomeView(BackendCandidateBase, TemplateView):
+    template_name = "backend_candidate/home.html"
 
     def get_context_data(self, *args, **kwargs):
         context = super(HomeView, self).get_context_data(*args, **kwargs)

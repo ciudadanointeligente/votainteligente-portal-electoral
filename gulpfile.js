624819a7584a9493dce8d3bec238d186df2fa9c5
Catalina Margozzini <ca.margozzini@gmail.com>
2016-08-05 15:42:56 -0300

    Secciones home

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,6 @@ var gulp = require('gulp'),
     watch = require('gulp-watch'),
     gutil = require('gulp-util');
 
-gulp.task('runserver', shell.task(['python manage.py runserver']));
-
 gulp.task('watch_compilescss', function () {
     shell.task(['python manage.py compilescss'])
     watch('votai_general_theme/static/sass/**/*.scss', function (vynil) {
@@ -13,4 +11,4 @@ gulp.task('watch_compilescss', function () {
     });
 });
 
-gulp.task('default', ['runserver', 'watch_compilescss']);
+gulp.task('default', ['watch_compilescss']);

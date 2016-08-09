5221a91e2ceb194738930ec48fd8dd0960fb3bf2
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2016-08-09 18:02:35 -0400

    Esto va para ti @camargozzini, ahora el gulp no se cae si hay errores en el sass y además el compilador de scss se corre al inciar el comando

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,20 @@
 var gulp = require('gulp'),
     shell = require('gulp-shell'),
     watch = require('gulp-watch'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    plumber = require('gulp-plumber');
+
+gulp.task('compilescss', function(){
+	shell.task(['python manage.py compilescss'])
+})
 
 gulp.task('watch_compilescss', function () {
-    shell.task(['python manage.py compilescss'])
+    
     watch('votai_general_theme/static/sass/**/*.scss', function (vynil) {
                                                gulp.src('votai_general_theme/static/sass/**/*.scss')
+                                               	   .pipe(plumber())
                                                    .pipe(shell(['python manage.py compilescss',]));
     });
 });
 
-gulp.task('default', ['watch_compilescss']);
+gulp.task('default', ['compilescss','watch_compilescss']);

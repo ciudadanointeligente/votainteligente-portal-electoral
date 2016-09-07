fa5acb8d496938c9ba2aac0bf1b957a79aad4c86
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2016-09-07 16:57:17 -0300

    Replicating the rest of the things

--- popular_proposal/replicator.py
+++ popular_proposal/replicator.py
@@ -16,6 +16,10 @@ class Replicator(object):
                                            clasification=self.proposal.clasification,
                                            title=self.proposal.title,
                                            created=datetime(day=19,month=8,year=2016),
+                                           background=self.proposal.background,
+                                           image=self.proposal.image,
+                                           document=self.proposal.document,
+                                           contact_details=self.proposal.contact_details,
                                            for_all_areas=True)
         self.proposal.for_all_areas = True
         self.proposal.save()

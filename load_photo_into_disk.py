876421e5e0c3289ff1e18d9486f53faeea0b4fad
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2016-09-09 16:50:25 -0300

    Agregando el que carga las imágenes desde uncsv

--- load_photo_into_disk.py
+++ load_photo_into_disk.py
@@ -7,17 +7,19 @@ from django.core.files.storage import default_storage
 from mimetypes import guess_extension
 from django.contrib.sites.models import Site
 from elections.models import Candidate
+import codecs
 
 
-current_site = Site.objects.get_current()
-for c in Candidate.objects.all().exclude(image__isnull=True).exclude(image__exact='').exclude(image__icontains=current_site.domain):
-
+def save_candidate_image_locally(c, url):
+    current_site = Site.objects.get_current()
     img_temp = NamedTemporaryFile(delete=True)
     try:
-        downloaded_image = urllib2.urlopen(c.image)
+        downloaded_image = urllib2.urlopen(url)
     except:
         print c.name, c.election
-        continue
+        # c.image = None
+        # c.save()
+        return
     d = downloaded_image.read()
     img_temp.write(d)
     img_temp.flush()
@@ -31,4 +33,31 @@ for c in Candidate.objects.all().exclude(image__isnull=True).exclude(image__exac
     url = u'http://' + current_site.domain + '/cache/' + file_name
     c.image = url
     c.save()
-    print c, c.image
+
+def process_all_candidates():
+    current_site = Site.objects.get_current()
+    for c in Candidate.objects.all()\
+            .exclude(image__isnull=True)\
+            .exclude(image__exact='')\
+            .exclude(image__icontains=current_site.domain):
+        save_candidate_image_locally(c, c.image)
+    
+
+
+def process_photo_csv():
+    reader = codecs.open("photos.csv", 'r', encoding='utf-8')
+    reader.next()
+    for line in reader:
+        row = line.split(u',')
+        candidate_name = row[1].title().strip()
+        image_url = row[2].title().strip()
+        if not candidate_name:
+            continue
+        if not image_url:
+            continue
+        try:
+            c = Candidate.objects.get(name=candidate_name)
+            save_candidate_image_locally(c, image_url)
+        except:
+            print u'No encontrado:' + candidate_name
+

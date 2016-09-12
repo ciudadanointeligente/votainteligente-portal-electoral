cdb071f586ba9d50e843eafdb7ac29b6bcd12bff
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2016-09-12 14:24:42 -0300

    Reducing the password generated to 12

--- backend_candidate/models.py
+++ backend_candidate/models.py
@@ -74,7 +74,7 @@ class CandidacyContact(models.Model):
         if self.candidacy is None:
             username = self.candidate.id + unicode(uuid.uuid4())[:4]
 
-            password = uuid.uuid4().hex
+            password = uuid.uuid4().hex[:12]
             self.initial_password = password
             user = User.objects.create(username=username)
             user.set_password(self.initial_password)

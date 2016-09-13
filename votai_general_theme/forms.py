c6768c31bb3002b7d256f982b7524854cd169089
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2016-09-13 13:38:55 -0300

    Initial values for candidate form

--- votai_general_theme/forms.py
+++ votai_general_theme/forms.py
@@ -3,11 +3,12 @@ from django import forms
 
 
 class PersonalDataForm(forms.Form):
-    age = forms.IntegerField(label='Edad', required=False)
-    lema = forms.CharField(label=u'Lema de campaña', required=False)
-    ocupacion = forms.CharField(label=u'Ocupación', required=False)
+    age = forms.IntegerField(label='Edad', required=False, initial=0)
+    lema = forms.CharField(label=u'Lema de campaña', required=False, initial='')
+    ocupacion = forms.CharField(label=u'Ocupación', required=False, initial='')
     experiencia = forms.CharField(label=u'Experiencia en Cargos públicos',
                                   widget=forms.Textarea(),
                                   max_length=4096,
-                                  required=False
+                                  required=False,
+                                  initial=''
                                   )

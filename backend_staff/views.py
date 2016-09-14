2be565ba6ca6b2e2690c79dcbf5ef99e53ef9654
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2016-09-14 15:47:07 -0300

    Ahora el agregar un mail y enviar aparece para los miembros del staff

--- backend_staff/views.py
+++ backend_staff/views.py
@@ -7,11 +7,13 @@ from popular_proposal.models import ProposalTemporaryData
 from preguntales.models import Message
 from django.views.generic.edit import FormView
 from popular_proposal.forms import CommentsForm, RejectionForm
+from backend_staff.forms import AddContactAndSendMailForm
 from django.shortcuts import get_object_or_404
 from django.core.urlresolvers import reverse
 from django.views.generic import DetailView, View
 from django.views.generic.detail import SingleObjectMixin
 from django.http import HttpResponseRedirect
+from elections.models import Candidate
 
 
 class IndexView(TemplateView):
@@ -109,3 +111,30 @@ class RejectPopularProposalView(FormView):
 
     def get_success_url(self):
         return reverse('backend_staff:index')
+
+class AddContactAndSendMailView(FormView):
+    form_class = AddContactAndSendMailForm
+    template_name = 'backend_staff/add_contact_and_send_mail.html'
+
+    @method_decorator(staff_member_required)
+    def dispatch(self, *args, **kwargs):
+        pk = self.kwargs.pop('pk')
+        self.candidate = get_object_or_404(Candidate, pk=pk)
+        return super(AddContactAndSendMailView, self).dispatch(*args, **kwargs)
+
+    def get_form_kwargs(self):
+        kwargs = super(AddContactAndSendMailView, self).get_form_kwargs()
+        kwargs['candidate'] = self.candidate
+        return kwargs
+
+    def form_valid(self, form):
+        form.send_mail()
+        return super(AddContactAndSendMailView, self).form_valid(form)
+
+    def get_success_url(self):
+        return self.candidate.get_absolute_url()
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(AddContactAndSendMailView, self).get_context_data(*args, **kwargs)
+        context['candidate'] = self.candidate
+        return context

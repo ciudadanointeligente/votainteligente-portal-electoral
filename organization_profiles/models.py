1f95249996d8f71cc81c92d5acee7c1c9f25eafd
damian-garrido <damian.garrido.munoz@gmail.com>
2017-05-11 16:30:13 -0300

    org template

--- organization_profiles/models.py
+++ organization_profiles/models.py
@@ -1,5 +1,8 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class OrganizationTemplate(models.Model):
+    organization = models.ForeignKey(User)

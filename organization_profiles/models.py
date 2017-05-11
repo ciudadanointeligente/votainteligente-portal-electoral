6311cc023c9b890f8078798e9348e2408641537c
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2017-05-11 16:51:53 -0300

    [WIP] agregando content a un template

--- organization_profiles/models.py
+++ organization_profiles/models.py
@@ -2,7 +2,18 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
+from backend_citizen.models import Profile
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
-# Create your models here.
+
 class OrganizationTemplate(models.Model):
     organization = models.ForeignKey(User)
+    content = models.TextField(default="FieraFeroz")
+
+
+@receiver(post_save, sender=Profile, dispatch_uid="create_user_profile")
+def create_organization_template(sender, instance, created, raw, **kwargs):
+    if raw:
+        return
+    template, created = OrganizationTemplate.objects.get_or_create(organization=instance.user)

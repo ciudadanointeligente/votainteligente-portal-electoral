45e7a9ecf03f21bb0d8b72c1c7a2fee22afa9b91
Lissett García Albornoz <lissett.garcia.a@gmail.com>
2017-05-16 12:18:32 -0300

    Arreglando el manso cagazo

--- organization_profiles/views.py
+++ organization_profiles/views.py
@@ -19,14 +19,14 @@ def read_template_as_string(path, file_source_path=__file__):
     return result
 
 
-compiler = Compiler()
-base_template =  compiler.compile(u'<!DOCTYPE html><html lang="es">{{> head}}<body><div>{{> nav}}<div class="container content_padding">{{> content}}</div></div>{{> footer}}</body></html>')
-head = compiler.compile(get_template("_head.html").render({}))
-footer = compiler.compile(get_template("_footer.html").render({}))
 
 
 class HandleBarsResponse(HttpResponse):
     def __init__(self, source, obj, **kwargs):
+        compiler = Compiler()
+        base_template =  compiler.compile(u'<!DOCTYPE html><html lang="es">{{> head}}<body><div>{{> nav}}<div class="container content_padding">{{> content}}</div></div>{{> footer}}</body></html>')
+        head = compiler.compile(get_template("_head.html").render({}))
+        footer = compiler.compile(get_template("_footer.html").render({}))
         content_template = compiler.compile(source)
         nav = compiler.compile(get_template("_navbar.html").render(obj))
         everything = base_template(obj, partials={"content": content_template,

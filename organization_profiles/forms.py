e97cacf7965c150a646ad5579611f0adc1a5ddc5
damian-garrido <damian.garrido.munoz@gmail.com>
2017-05-16 17:51:59 -0400

    [WIP] organization profile

--- organization_profiles/forms.py
+++ organization_profiles/forms.py
@@ -1,2 +1,8 @@
-class OrganizationTemplateForm():
-    pass
+# coding=utf-8
+from django import forms
+from organization_profiles.models import OrganizationTemplate, BASIC_FIELDS
+
+class OrganizationTemplateForm(forms.ModelForm):
+    class Meta:
+        model = OrganizationTemplate
+        fields = BASIC_FIELDS

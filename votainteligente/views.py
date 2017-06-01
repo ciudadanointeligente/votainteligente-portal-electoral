fc590cd7edeca5a97591b6dfc8999812b9d2a8fc
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2017-06-01 13:09:44 -0400

    [WIP] logging a user in automatically after activation. The problem was not solved and the reason can be found at ubernostrum/django-registration#92

--- votainteligente/views.py
+++ votainteligente/views.py
@@ -1,5 +1,15 @@
 from django.views.generic import TemplateView
+from django.dispatch import receiver
+from registration.signals import user_activated
+from django.contrib.auth import login
+from django.shortcuts import redirect
+
+
+@receiver(user_activated)
+def post_activated_user(sender, user, request, **kwargs):
+    login(request, user, backend='registration.backends.hmac')
+    redirect('/')
 
 
 class HomeViewBase(TemplateView):
-	template_name='index.html'
+    template_name = 'index.html'

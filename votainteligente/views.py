1464e1a175904a4b999ad772d2edf3eff3975164
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2017-06-06 10:21:13 -0400

    Autologin user after validation

--- votainteligente/views.py
+++ votainteligente/views.py
@@ -2,13 +2,12 @@ from django.views.generic import TemplateView
 from django.dispatch import receiver
 from registration.signals import user_activated
 from django.contrib.auth import login
-from django.shortcuts import redirect
 
 
 @receiver(user_activated)
 def post_activated_user(sender, user, request, **kwargs):
-    login(request, user, backend='registration.backends.hmac')
-    redirect('/')
+    user.backend = 'django.contrib.auth.backends.ModelBackend'
+    login(request, user)
 
 
 class HomeViewBase(TemplateView):

c7f749b317b2a76b5d21e4675fbf7b1397fe8ed1
damian-garrido <damian.garrido.munoz@gmail.com>
2017-06-07 11:52:45 -0400

    placeholders for Organization profile, close #704

--- organization_profiles/forms.py
+++ organization_profiles/forms.py
@@ -4,8 +4,30 @@ from organization_profiles.models import OrganizationTemplate, BASIC_FIELDS
 
 
 class OrganizationTemplateForm(forms.ModelForm):
-    primary_color = forms.CharField(widget=forms.TextInput(attrs={'type': 'color'}))
-    secondary_color = forms.CharField(widget=forms.TextInput(attrs={'type': 'color'}))
+    title = forms.CharField(label="Nombre de tu organización",
+                            widget=forms.TextInput(attrs={'placeholder': 'Fundación Ciudadano Inteligente'}))
+    sub_title = forms.CharField(label="Nombre de tu organización",
+                            widget=forms.Textarea(attrs={'placeholder': 'Ayúdanos a que te conozcan! En esta sección podrás contarnos sobre tu organización para informar a otros sobre tu labor social.'}))
+    org_url = forms.URLField(label="Web de tu organización",
+                             required=False,
+                            widget=forms.TextInput(attrs={'placeholder': 'https://miorganizacion.org'}))
+    facebook = forms.URLField(label="FanPage de tu organización",
+                              required=False,
+                            widget=forms.TextInput(attrs={'placeholder': 'https://www.facebook.com/miorganizacion/'}))
+    twitter = forms.URLField(label="Twitter de tu organización",
+                             required=False,
+                            widget=forms.TextInput(attrs={'placeholder': 'https://www.twitter.com/miorganizacion/'}))
+    instagram = forms.URLField(label="Instagram de tu organización",
+                               required=False,
+                            widget=forms.TextInput(attrs={'placeholder': 'https://www.instagram.com/miorganizacion/'}))
+    rss_url = forms.URLField(label="RSS de tu organización",
+                             required=False,
+                            widget=forms.TextInput(attrs={'placeholder': 'https://miorganizacion.org/rss.xml'}))
+
+    primary_color = forms.CharField(widget=forms.TextInput(attrs={'placeholder': '#CCDDCC',
+                                                                  'type': 'color'}))
+    secondary_color = forms.CharField(widget=forms.TextInput(attrs={'placeholder': '#DDCCDD',
+                                                                  'type': 'color'}))
     class Meta:
         model = OrganizationTemplate
         fields = BASIC_FIELDS

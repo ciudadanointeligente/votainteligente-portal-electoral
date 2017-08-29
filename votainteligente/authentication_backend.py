2b92b5a2252d97fba6364709d6458831e7089ce5
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2017-08-29 17:33:07 -0300

    Los usuarios se pueden loggear utilizando su email. Esto es una estricta medida para la flexibilidad contra los pasteles.

---
+++ votainteligente/authentication_backend.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth import get_user_model
+from django.forms import ValidationError
+from django.core.validators import validate_email
+
+
+class VotaIAuthenticationBackend(ModelBackend):
+    """
+    Authenticates against settings.AUTH_USER_MODEL.
+    """
+
+    def authenticate(self, username=None, password=None, **kwargs):
+        UserModel = get_user_model()
+        if username is None:
+            username = kwargs.get(UserModel.USERNAME_FIELD)
+        try:
+            try:
+                validate_email(username)
+                kwargs = {'email': username}
+            except ValidationError:
+                kwargs = {'username': username}
+            user = UserModel._default_manager.get(**kwargs)
+
+        except UserModel.DoesNotExist:
+            # Run the default password hasher once to reduce the timing
+            # difference between an existing and a non-existing user (#20760).
+            UserModel().set_password(password)
+        else:
+            if user.check_password(password) and self.user_can_authenticate(user):
+                return user

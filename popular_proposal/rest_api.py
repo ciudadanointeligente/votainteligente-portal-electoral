4a4344709cc3beaa36b2b6234b8da727e679ccd7
damian-garrido <damian.garrido.munoz@gmail.com>
2017-10-03 11:14:36 -0300

    adding created field to the API response

--- popular_proposal/rest_api.py
+++ popular_proposal/rest_api.py
@@ -9,7 +9,7 @@ class ProposalSerializer(HyperlinkedModelSerializer):
     proposer = StringRelatedField()
     class Meta:
         model = PopularProposal
-        fields = ('id','title', 'slug', 'get_absolute_url', 'data', 'proposer')
+        fields = ('id','title', 'slug', 'get_absolute_url', 'data', 'proposer','created')
 
 class ProposalViewSet(ReadOnlyModelViewSet):
     queryset = PopularProposal.objects.all()

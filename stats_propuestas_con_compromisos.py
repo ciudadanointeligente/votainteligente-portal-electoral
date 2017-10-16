dd5b53a8505e0d98eb9770190e9c0d5917d60fba
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2017-10-16 16:13:33 -0300

    Agregando calculos

---
+++ stats_propuestas_con_compromisos.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+import os
+import django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "votainteligente.settings")
+django.setup()
+import pprint
+from popular_proposal.models import PopularProposal
+from elections.models import Election
+for p in PopularProposal.objects.exclude(commitments__isnull=True):
+    c = p.commitments.count()
+    print unicode(p.id) + u"|" +p.get_classification()+ u"|" + p.title + u"|" + unicode(c)

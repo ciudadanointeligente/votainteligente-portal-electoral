96f8e3a6726caa1dd440c1bba01a22af6407932d
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2017-10-23 11:51:05 -0300

    El corazón esta listo!

--- medianaranja2/proposals_getter.py
+++ medianaranja2/proposals_getter.py
@@ -10,3 +10,16 @@ class ProposalsGetter(object):
     def proposals(self, election):
         commitments = Commitment.objects.filter(commited=True, candidate__elections=election)
         return list(PopularProposal.objects.filter(commitments__in=commitments).distinct())
+
+    def get_all_proposals(self, area):
+        proposals = []
+        has_parent = True
+        while has_parent:
+            if area.elections.all():
+                for election in area.elections.all():
+                    proposals += self.proposals(election)
+            if not area.parent:
+                has_parent = False
+            else:
+                area = area.parent
+        return proposals
--- medianaranja2/tests/proposals_selector_tests.py
+++ medianaranja2/tests/proposals_selector_tests.py
@@ -1,5 +1,10 @@
 from .adapter_tests import MediaNaranjaAdaptersBase
 from medianaranja2.proposals_getter import ProposalsGetter
+from elections.models import Area, Election, Candidate
+from django.contrib.auth.models import User
+from popular_proposal.models import (PopularProposal,
+                                     Commitment,
+                                     )
 
 
 class ProposalsGetterTestCase(MediaNaranjaAdaptersBase):
@@ -13,3 +18,56 @@ class ProposalsGetterTestCase(MediaNaranjaAdaptersBase):
         self.assertIn(self.p1, proposals)
         self.assertIn(self.p2, proposals)
         self.assertIn(self.p3, proposals)
+
+    def test_several_proposals_per_election(self):
+        child = Area.objects.create(name="children")
+        self.election.area = child
+        self.election.save()
+        mother = Area.objects.create(name="mother")
+        mother.children.add(child)
+        child.children.add(mother)
+        e1 = Election.objects.create(name='the election_mother',
+                                     area=mother,
+                                     position='jefe')
+        c_1_1 = Candidate.objects.create(name="c_1_1")
+        e1.candidates.add(c_1_1)
+        c_1_2 = Candidate.objects.create(name="c_1_2")
+        e1.candidates.add(c_1_2)
+        grand_mother = Area.objects.create(name="grand_mother")
+        grand_mother.children.add(mother)
+
+        e2 = Election.objects.create(name='the election_grand_mother',
+                                     area=grand_mother,
+                                     position='super jefe')
+        c_2_1 = Candidate.objects.create(name="c_2_1")
+        e2.candidates.add(c_2_1)
+        c_2_2 = Candidate.objects.create(name="c_2_2")
+        e2.candidates.add(c_2_2)
+
+        proposer = User.objects.create_user(username="dog_proposer")
+        pa = PopularProposal.objects.create(proposer=proposer,
+                                            title=u'pA',
+                                            data={}
+                                            )
+        pb = PopularProposal.objects.create(proposer=proposer,
+                                            title=u'PB',
+                                            data={}
+                                            )
+        pc = PopularProposal.objects.create(proposer=proposer,
+                                            title=u'PC',
+                                            data={}
+                                            )
+
+        Commitment.objects.create(candidate=c_1_1, proposal=pa, commited=True)
+        Commitment.objects.create(candidate=c_2_2, proposal=pb, commited=True)
+        Commitment.objects.create(candidate=c_2_2, proposal=pb, commited=True)
+
+        getter = ProposalsGetter()
+        proposed_proposals = getter.get_all_proposals(child)
+        self.assertIn(self.p1, proposed_proposals)
+        self.assertIn(self.p2, proposed_proposals)
+        self.assertIn(self.p3, proposed_proposals)
+        self.assertIn(pa, proposed_proposals)
+        self.assertIn(pb, proposed_proposals)
+        self.assertNotIn(pc, proposed_proposals)
+        self.assertEquals(len(proposed_proposals), 5)

1f648c336168330e1a57fd2a3810f6f1d90fdfb0
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2017-10-25 16:05:58 -0300

    Formularios para la 1/2 naranja por separado

--- medianaranja2/forms.py
+++ medianaranja2/forms.py
@@ -8,3 +8,30 @@ from django.conf import settings
 class SetupForm(forms.Form):
     area = forms.ModelChoiceField(queryset=Area.objects.filter(classification__in=settings.FILTERABLE_AREAS_TYPE))
     categories = forms.ModelMultipleChoiceField(queryset=QuestionCategory.objects.all())
+
+
+class QuestionsForm(forms.Form):
+
+    def __init__(self, *args, **kwargs):
+        self.categories = kwargs.pop('categories')
+        super(QuestionsForm, self).__init__(*args, **kwargs)
+        for category in self.categories:
+            for topic in category.topics.order_by('id'):
+                self.fields[topic.slug] = forms.ModelChoiceField(queryset=topic.positions)
+
+    def clean(self):
+        cleaned_data = super(QuestionsForm, self).clean()
+        cleaned_data['positions'] = []
+        for topic in cleaned_data:
+            cleaned_data['positions'].append(cleaned_data[topic])
+
+        return cleaned_data
+
+class ProposalsForm(forms.Form):
+    proposals = forms.ModelMultipleChoiceField(queryset=PopularProposal.objects.none())
+
+    def __init__(self, *args, **kwargs):
+        self.proposals = kwargs.pop('proposals')
+        super(ProposalsForm, self).__init__(*args, **kwargs)
+        qs = PopularProposal.objects.filter(id__in=[p.id for p in self.proposals])
+        self.fields['proposals'].queryset = qs
--- medianaranja2/tests/forms_tests.py
+++ medianaranja2/tests/forms_tests.py
@@ -1,8 +1,9 @@
 # coding=utf-8
 from .adapter_tests import MediaNaranjaAdaptersBase
 from elections.models import Area
-from medianaranja2.forms import SetupForm
+from medianaranja2.forms import SetupForm, QuestionsForm, ProposalsForm
 from django.conf import settings
+from django import forms
 
 
 class FormsTestCase(MediaNaranjaAdaptersBase):
@@ -25,3 +26,46 @@ class FormsTestCase(MediaNaranjaAdaptersBase):
         self.assertEquals(form.cleaned_data['area'], self.area)
         self.assertIn(self.category1, form.cleaned_data['categories'])
         self.assertIn(self.category2, form.cleaned_data['categories'])
+
+    def test_questions_form_instanciate(self):
+        kwargs = {'categories': [self.category1, self.category2]}
+        form = QuestionsForm(**kwargs)
+        self.assertEquals(len(form.fields), 2)
+        self.assertIn(self.topic1.slug, form.fields)
+        self.assertIsInstance(form.fields[self.topic1.slug], forms.ModelChoiceField)
+        topic_1_choices = list(form.fields[self.topic1.slug].choices)
+        self.assertIn((self.position1.id, unicode(self.position1)), topic_1_choices)
+        self.assertIn((self.position2.id, unicode(self.position2)), topic_1_choices)
+        self.assertIn(self.topic2.slug, form.fields)
+        self.assertIsInstance(form.fields[self.topic2.slug], forms.ModelChoiceField)
+        topic_2_choices = list(form.fields[self.topic2.slug].choices)
+        self.assertIn((self.position3.id, unicode(self.position3)), topic_2_choices)
+        self.assertIn((self.position4.id, unicode(self.position4)), topic_2_choices)
+
+    def test_get_cleaned_data_from_questions_form(self):
+        data = {self.topic1.slug: self.position1.id,
+                self.topic2.slug: self.position3.id}
+        kwargs = {'categories': [self.category1, self.category2], 'data': data}
+        form = QuestionsForm(**kwargs)
+        self.assertTrue(form.is_bound)
+        self.assertTrue(form.is_valid())
+        self.assertIn(self.position1, form.cleaned_data['positions'])
+        self.assertIn(self.position3, form.cleaned_data['positions'])
+
+    def test_proposals_form_instanciate(self):
+        kwargs = {'proposals': [self.p1, self.p2, self.p3]}
+        form = ProposalsForm(**kwargs)
+        proposals_choices = list(form.fields['proposals'].choices)
+        self.assertIn((self.p1.id, unicode(self.p1)), proposals_choices)
+        self.assertIn((self.p2.id, unicode(self.p2)), proposals_choices)
+        self.assertIn((self.p3.id, unicode(self.p3)), proposals_choices)
+
+    def test_posting_proposals_form(self):
+        data = {'proposals': [self.p1.id, self.p2.id]}
+        kwargs = {'proposals': [self.p1, self.p2, self.p3], 'data':data}
+        form = ProposalsForm(**kwargs)
+        self.assertTrue(form.is_bound)
+        self.assertTrue(form.is_valid())
+        self.assertIn(self.p1, form.cleaned_data['proposals'])
+        self.assertIn(self.p1, form.cleaned_data['proposals'])
+        

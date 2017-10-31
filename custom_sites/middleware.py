11062dec04bba1d53a50d0bd7e94a960f4d21288
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2017-10-31 14:43:01 -0300

    mejorando el home en terminos de cache

--- custom_sites/middleware.py
+++ custom_sites/middleware.py
@@ -4,14 +4,21 @@ from django.utils.deprecation import MiddlewareMixin
 from django.contrib.sites.shortcuts import get_current_site
 from django.contrib.sites.models import Site
 from custom_sites.models import CustomSite
+from django.core.cache import cache
 
 
 class VotaIcurrentSiteMiddleware(MiddlewareMixin):
     def process_request(self, request):
         try:
-            site = Site.objects.get(domain=request.get_host())
-            custom = CustomSite.objects.get(site=site)
-            request.site = site
-            request.urlconf = custom.urlconf
+            host = request.get_host()
+            cache_key = "custom_site_" + host
+            site_and_custom = cache.get(cache_key)
+            if site_and_custom is None:
+                site = Site.objects.get(domain=host)
+                custom = CustomSite.objects.get(site=site)
+                site_and_custom = (site, custom)
+                cache.set(cache_key, site_and_custom)
+            request.site = site_and_custom[0]
+            request.urlconf = site_and_custom[1].urlconf
         except (Site.DoesNotExist, CustomSite.DoesNotExist):
             request.site = get_current_site(request)

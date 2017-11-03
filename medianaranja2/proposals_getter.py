509fd7aea8ce8f6e81f694eb8997cf4832daacb3
Luis Felipe Álvarez Burgos <falvarez@ciudadanointeligente.org>
2017-11-03 10:31:06 -0300

    Updating the proposals getter

--- medianaranja2/proposals_getter.py
+++ medianaranja2/proposals_getter.py
@@ -13,20 +13,25 @@ class ProposalsGetter(object):
         commitments = Commitment.objects.filter(commited=True, candidate__elections=election)
         return list(PopularProposal.objects.filter(commitments__in=commitments).distinct())
 
-    def get_all_proposals(self, area):
-        cache_key = 'proposals_for_' + area.id
-        if cache.get(cache_key) is not None:
-            return cache.get(cache_key)
-        proposals = []
+    def get_elections(self, area):
         has_parent = True
+        elections = []
         while has_parent:
             if area.elections.all():
-                for election in area.elections.all():
-                    proposals += self.proposals(election)
+                elections += list(area.elections.all())
             if not area.parent:
                 has_parent = False
             else:
                 area = area.parent
-        proposals = sorted(proposals, key=lambda p: -p.likers.count())
+        return elections
+
+
+    def get_all_proposals(self, area):
+        cache_key = 'proposals_for_' + area.id
+        if cache.get(cache_key) is not None:
+            return cache.get(cache_key)
+        elections = self.get_elections(area)
+        proposals = PopularProposal.ordered.filter(commitments__candidate__elections__in=elections)
+        proposals = list(proposals[:config.MEDIA_NARANJA_MAX_NUM_PR])
         cache.set(cache_key, proposals)
         return proposals[:config.MEDIA_NARANJA_MAX_NUM_PR]

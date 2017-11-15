5ed02caab5d7542cb3400e290257af4e62352925
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2017-11-15 18:01:09 -0300

    Adding throtling to api

--- votainteligente/votainteligente_settings.py
+++ votainteligente/votainteligente_settings.py
@@ -352,6 +352,14 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 30,
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '10/day',
+        'user': '100/day'
+    }
 }
 
 THEME = None

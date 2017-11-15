91a5591eac0e796bb2e266843ea32174c7dba93f
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2017-11-15 16:50:41 -0300

    quitando el csv

--- votainteligente/votainteligente_settings.py
+++ votainteligente/votainteligente_settings.py
@@ -347,7 +347,7 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
-        'rest_framework_csv.renderers.CSVRenderer',
+        # 'rest_framework_csv.renderers.CSVRenderer',
     ),
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
 }

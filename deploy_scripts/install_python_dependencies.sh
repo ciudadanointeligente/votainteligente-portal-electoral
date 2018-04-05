1c01cb1758c33a1d8a04afd04494d9e6b486a03b
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2018-04-05 19:08:45 -0300

    Cambiando appspec

--- deploy_scripts/install_python_dependencies.sh
+++ deploy_scripts/install_python_dependencies.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-source ~/.virtualenvs/vota/bin/activate
 cd ~/vota/
 pip install -r requirements.txt
--- deploy_scripts/migrate_and_restart.sh
+++ deploy_scripts/migrate_and_restart.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-source ~/.virtualenvs/vota/bin/activate
 cd ~/vota/
 python manage.py compilescss
 python manage.py collectstatic --noinput

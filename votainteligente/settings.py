5def00bd03bc7741cfc9eb3d67f56bb97b20a824
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2018-04-06 16:24:45 -0300

    Agregando bootstrap4 a installed_apps

--- votainteligente/settings.py
+++ votainteligente/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = (
     'django.contrib.sitemaps',
     'linaro_django_pagination',
     'bootstrap3',
+    'bootstrap4',
     'formtools',
     'robots',
     # "registration_defaults",

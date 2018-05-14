f5cf51ca8a0a47e964f40050c8adbb05c232b84a
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2018-05-14 16:55:19 -0300

    Categorías de propuesta para rioporinteiro

---
+++ rioxinteiro/proposals_categories.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+from django.utils.translation import ugettext as _
+
+_TOPIC_CHOICES = (('', _(u'Selecciona una categoría')),
+                 (u"politica-metropolitana", u"Política Metropolitana"),
+                 (u"emprego-renda", u"Emprego & Renda"),
+                 (u"moradia", u"Moradia "),
+                 (u"mobilidade-urbana", u"Mobilidade Urbana"),
+                 (u"seguranca-publica", u"Segurança Pública"),
+                 (u"saneamento-basico", u"Saneamento Básico"),
+                 (u"saude", u"Saúde"),
+                 (u"educacao", u"Educação"),
+                 (u"cultura", u"Cultura"),
+                 (u"espacos-publicos", u"Espaços Públicos"),
+                 (u"meio-ambiente", u"Meio Ambiente  "),
+                 (u"direitos-humanos", u"Direitos Humanos"),
+                 (u"justica-racial", u"Justiça Racial "),
+                 (u"equidade-de-genero", u"Equidade de Gênero"),
+                 (u"transparencia-participacao-e-sistema-politico", u"Transparência, Participação e Sistema Político "),
+                 (u"juventude", u"Juventude"),
+                 (u"infancia", u"Infância"),
+
+                 )

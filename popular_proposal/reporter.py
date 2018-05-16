c85aaaa54b5bc837ba5c84320466a4ffb6f45a59
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2018-05-16 18:20:54 -0300

    Agregando recomendaciones para organizaciones

--- popular_proposal/reporter.py
+++ popular_proposal/reporter.py
@@ -1,5 +1,8 @@
 from votai_utils.send_mails import send_mail
 from django.conf import settings
+from popular_proposal.models import PopularProposal
+import datetime
+from django.utils import timezone
 
 
 class PeriodicReporter(object):
@@ -11,8 +14,7 @@ class PeriodicReporter(object):
     def get_proposals(self):
         return self.user.proposals.all()
 
-    def get_mail_context(self):
-        context = {'user': self.user}
+    def get_proposals_dict(self):
         proposals_dict = []
         for proposal in self.get_proposals():
             proposal_dict = {
@@ -20,7 +22,24 @@ class PeriodicReporter(object):
                 'analytics': proposal.get_analytics(days=self.days)
             }
             proposals_dict.append(proposal_dict)
-        context['proposals'] = proposals_dict
+        return proposals_dict
+
+    def get_new_proposals(self):
+        classifications = [p.clasification for p in self.get_proposals()]
+        since_when = timezone.now() - datetime.timedelta(days=self.days)
+        proposals = PopularProposal.objects.exclude(proposer=self.user)
+        proposals = proposals.exclude(likers=self.user)
+        proposals = proposals.filter(clasification__in=classifications)
+        proposals = proposals.filter(created__gte=since_when)
+        return proposals
+
+    def get_mail_context(self):
+        context = {'user': self.user}
+
+        context['proposals'] = self.get_proposals_dict()
+        if self.user.profile.is_organization:
+            context['new_proposals'] = self.get_new_proposals()
+            print context['new_proposals']
         return context
 
     def send_mail(self):

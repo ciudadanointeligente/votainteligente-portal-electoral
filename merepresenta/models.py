07bbe1ac114c56b0aa63aa6776e76f62f49981bc
Felipe Álvarez <luisfelipealvarez@gmail.com>
2018-07-06 13:20:21 -0300

    Ordering done

--- merepresenta/models.py
+++ merepresenta/models.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.db.models import Case, Value, When, PositiveSmallIntegerField
+from django.db.models import Case, Value, When, PositiveSmallIntegerField, Sum, F
 from popular_proposal.models import PopularProposal, Commitment
 from elections.models import Candidate as OriginalCandidate
 from django.core.urlresolvers import reverse
@@ -21,14 +21,24 @@ class MeRepresentaCommitment(Commitment):
         return self._save(*args, **kwargs)
 
 
+NON_MALE_KEY = "F"
+NON_WHITE_KEY = {"possible_values": ["preta", "parda"]}
+
+
 class ForVolunteersManager(models.Manager):
     def get_queryset(self):
         qs = super(ForVolunteersManager, self).get_queryset()
         qs = qs.annotate(
                         is_women=Case(When(gender='F', then=Value(1)),
-                                              default=Value(0),
-                                              output_field=PositiveSmallIntegerField())
+                        default=Value(0),
+                        output_field=PositiveSmallIntegerField())
                     )
+        qs = qs.annotate(is_non_white=Case(When(personal_datas__value__in=NON_WHITE_KEY['possible_values'], then=Value(1)),
+                        default=Value(0),
+                        output_field=PositiveSmallIntegerField())
+
+                )
+        qs = qs.annotate(desprivilegio=F('is_women') + F('is_non_white'))
         return qs
 
 
--- merepresenta/tests/volunteers_candidate_ordering_tests.py
+++ merepresenta/tests/volunteers_candidate_ordering_tests.py
@@ -1,7 +1,8 @@
 # coding=utf-8
 from django.test import TestCase, override_settings
-from merepresenta.models import Candidate
+from merepresenta.models import Candidate, NON_WHITE_KEY, NON_MALE_KEY
 from elections.tests import VotaInteligenteTestCase as TestCase
+from elections.models import PersonalData
 
 
 class CandidateOrderingTests(TestCase):
@@ -10,9 +11,29 @@ class CandidateOrderingTests(TestCase):
 
 
 	def test_women_have_an_extra_value(self):
-		Candidate.objects.filter(id__in=[4, 5]).update(gender='F')
+		Candidate.objects.filter(id__in=[4, 5]).update(gender=NON_MALE_KEY)
 		cs = Candidate.objects.filter(id__in=[4,5])
 		self.assertEquals(cs[0].is_women, 1)
 		self.assertEquals(cs[1].is_women, 1)
 
+	def test_race_is_also_an_extra_value(self):
+		c = Candidate.objects.get(id=5)
+		personal_data = PersonalData.objects.create(label=u'Cor e raça',
+                                                    value=NON_WHITE_KEY["possible_values"][0],
+                                                    candidate=c)
+		cs = Candidate.objects.filter(id__in=[c.id])
+		self.assertEquals(cs[0].is_non_white, 1)
 
+	def test_it_sums_values(self):
+		Candidate.objects.filter(id__in=[4, 5]).update(gender=NON_MALE_KEY)
+		c = Candidate.objects.get(id=5)
+		personal_data = PersonalData.objects.create(label=u'Cor e raça',
+                                                    value=NON_WHITE_KEY["possible_values"][0],
+                                                    candidate=c)
+		
+		self.assertEquals(Candidate.objects.get(id=5).desprivilegio, 2)
+		self.assertEquals(Candidate.objects.get(id=4).desprivilegio, 1)
+		
+		cs = Candidate.objects.all().order_by('-desprivilegio')
+		self.assertEquals(int(cs[0].id), 5)
+		self.assertEquals(int(cs[1].id), 4)

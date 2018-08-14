6b535972233d0b6ff9aacfb56515db40c118cadb
Felipe Álvarez <luisfelipealvarez@gmail.com>
2018-08-14 15:52:03 -0300

    Agregando unos datos que me dijo la malu

--- rioxinteiro/forms.py
+++ rioxinteiro/forms.py
@@ -5,6 +5,9 @@ from django import forms
 class PersonalDataForm(forms.Form):
     lema = forms.CharField(label=u'Slogan da campanha', required=False, initial='')
     ocupacion = forms.CharField(label=u'Ocupação atual', required=False, initial='')
+    raca = forms.CharField(label=u'Raça', required=False, initial='')
+    orietacion_sexual = forms.CharField(label=u'Orientação sexual', required=False, initial='')
+    territorio = forms.CharField(label=u'Território', required=False, initial='')
     experiencia = forms.CharField(label=u'Bio',
                                   widget=forms.Textarea(),
                                   max_length=4096,

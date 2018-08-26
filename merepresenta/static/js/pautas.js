ce7440a6e7a7a72b55a19b87b8b52b9be3d582eb
Miguel <oieusouamiguel@gmail.com>
2018-08-26 05:14:48 -0300

    pautas: refactor pautas version 1.1

--- merepresenta/static/js/pautas.js
+++ merepresenta/static/js/pautas.js
@@ -45,15 +45,15 @@ $(function(){
 
 });
 
-
-
-$(".tab :first").show();
 $(function() {
-    $(".tabs-menu a").click(function(event) {
-        event.preventDefault();
+
+    let $tabs_menu = $(".tabs-menu a")
+
+    $tabs_menu.click(function() {
         $(this).parent().addClass("current");
-        $(this).parent().siblings().removeClass("current");
-        var tab = $(this).attr("href");
+        $(this).parent().siblings().removeClass("current"); 
+        
+        let tab = $(this).attr("href");
         $(".tab-content").not(tab).css("display", "none");
         $(tab).fadeIn();
     });

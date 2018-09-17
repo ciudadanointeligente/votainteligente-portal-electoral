4c509b4233ca2ffa770784e2ca18e0bc6336dc91
Felipe Álvarez <luisfelipealvarez@gmail.com>
2018-09-17 15:00:12 -0300

    Arreglando el perfil de un candidato

--- merepresenta/match/matrix_builder.py
+++ merepresenta/match/matrix_builder.py
@@ -139,6 +139,6 @@ class MatrixBuilder(object):
             i = self.candidates_id[c.id]
             mark = r[i]
             d = self.candidates_dict[c.id]
-            d['nota'] = float(mark)/float(100)
+            d['nota'] = round(float(mark)/float(100), 2)
             as_array.append(d)
         return as_array
